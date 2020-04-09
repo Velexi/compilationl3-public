c2d76a2057416f3d9845a43e4852f742d590dd77
Velexi <faycal.henis@yahoo.fr>
2020-04-09 22:06:53 +0200

    ts

--- src/Sa2ts.java
+++ src/Sa2ts.java
@@ -1,5 +1,7 @@
 import sa.*;
 import ts.Ts;
+import ts.TsItemFct;
+import ts.TsItemVar;
 
 public class Sa2ts extends SaDepthFirstVisitor<Void> {
 
@@ -9,99 +11,173 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
 
     Ts globalTable = new Ts();
     Ts currentTable;
-    private int indetation = 0;
+    private int indentation = 0;
     @Override
     public void defaultIn(SaNode node) {
-        for (int i = 0 ; i< indetation ; i++)
+        for (int i = 0; i< indentation; i++)
             System.out.print(" ");
         System.out.println("<"+node.getClass().getSimpleName()+">");
-        indetation++;
+        indentation++;
     }
 
     @Override
     public void defaultOut(SaNode node) {
-        indetation--;
-        for (int i = 0 ; i< indetation ; i++)
+        indentation--;
+        for (int i = 0; i< indentation; i++)
             System.out.print(" ");
         System.out.println("</"+node.getClass().getSimpleName()+">");
     }
 
     @Override
     public Void visit(SaDecTab node) {
-        if(globalTable.getVar(node.getNom()) != null) throw new RuntimeException("le tableau existe déjà");
-        node.tsItem = globalTable.addVar(node.getNom(),node.getTaille());
+        defaultIn(node);
+        String nom = node.getNom();
+        if(variableExists(nom,globalTable)) throw new RuntimeException(nom+"existe déjà");
+        int taille = node.getTaille();
+        if(taille < 2) throw new IllegalArgumentException("La taille d'un tableau doit être superieure ou égale à 2");
+        node.tsItem = globalTable.addVar(nom,taille);
+        defaultOut(node);
         return null;
 
     }
 
     @Override
     public Void visit(SaDecFonc node) {
-
-        if(globalTable.getFct(node.getNom()) != null) throw new RuntimeException("la fonction existe déja");
-
-        Ts localTable = new Ts();
-        int nbParam = node.getParametres() == null ? 0 : node.getParametres().length();
-        int nbVar = node.getVariable() == null ? 0 : node.getVariable().length();
-
-
-        SaDec tete =nbParam == 0 ? null : node.getParametres().getTete();
-        SaLDec queue= nbParam == 0 ? null : node.getParametres().getQueue();
-
-        for(int index = 0 ; index < nbParam; index++){
-            if(localTable.getVar(tete.getNom()) != null)throw new RuntimeException("variable existe");
-            if(tete instanceof SaDecTab)throw new RuntimeException("pas de tableau frr");
-            localTable.addParam(tete.getNom());
-            tete = queue.getTete();
-            if(queue.getQueue() == null) break;
-            queue = queue.getQueue();
-
-        }
-
-        tete = nbVar ==0 ? null : node.getVariable().getTete();
-        queue = nbVar==0 ? null : node.getVariable().getQueue();
-
-
-
-        for(int index = 0 ; index < nbVar; index++){
-            if(localTable.getVar(tete.getNom()) != null)throw new RuntimeException("variable existe");
-            if(tete instanceof SaDecTab)throw new RuntimeException("pas de tableau frr");
-            localTable.addVar(tete.getNom(),1);
-            tete = queue.getTete();
-            if(queue.getQueue() == null) break;
-            queue = queue.getQueue();
-
-        }
-        node.tsItem = globalTable.addFct(node.getNom(),nbParam,localTable,node);
-        currentTable = localTable;
+        defaultIn(node);
+        if (functionExists(node.getNom())) throw new RuntimeException("Fonction " + node.getNom() + " déja définie");
+        if (!addFunction(node)) return null;
+        currentTable = globalTable.getTableLocale(node.getNom());
         node.getCorps().accept(this);
+        defaultOut(node);
         return null;
     }
 
     @Override
     public Void visit(SaDecVar node) {
-        return super.visit(node);
+        defaultIn(node);
+        if(variableExists(node.getNom(),globalTable)) throw new RuntimeException(node.getNom()+"est déjà déclarée");
+        node.tsItem = globalTable.addVar(node.getNom(),1);
+        defaultOut(node);
+        return null;
     }
 
     @Override
     public Void visit(SaVarSimple node) {
-        if(globalTable.getVar(node.getNom()) != null) throw new RuntimeException("la variable existe déjà");
-        node.tsItem = globalTable.addVar(node.getNom(),1);
-        return null;
+        defaultIn(node);
+       boolean estGlob = false;
+       if(variableDoesntExist(node.getNom(),currentTable)){
+           if(variableDoesntExist(node.getNom(),globalTable)){
+               variableIsntDefined(node.getNom());
+           }else {
+               estGlob = true;
+           }
+       }
+
+        TsItemVar var;
+       if(estGlob){
+           var = globalTable.getVar(node.getNom());
+       }else {
+           var = currentTable.getVar(node.getNom());
+       }
+
+       if(var.taille > 1)
+           throw new RuntimeException("Mauvaise declaration d'un tableau");
+       node.tsItem = var;
+       return null;
 
     }
 
     @Override
     public Void visit(SaAppel node) {
-        return super.visit(node);
-    }
+        defaultIn(node);
+
+        String nom = node.getNom();
+        int nbrArguments = node.getArguments().length();
+
+        TsItemFct fct = globalTable.getFct(nom);
+
+        if(!functionExists(nom)) throw new RuntimeException("Cette fonction "+nom+" n'existe pas");
+        if(nbrArguments != fct.nbArgs) throw new RuntimeException("problème d'arguments");
+
+        node.tsItem = fct;
+        return null;
+     }
 
     @Override
     public Void visit(SaVarIndicee node) {
-        if(currentTable.getVar(node.getNom()) == null) throw new RuntimeException("le tableau n'existe pas");
-        node.tsItem = currentTable.getVar(node.getNom());
-        return null;
+      if(variableExists(node.getNom(),currentTable)) throw new RuntimeException(node.getNom()+"est une var locale");
+      if(variableDoesntExist(node.getNom(),globalTable)) throw new RuntimeException(node.getNom()+" non déclarée ");
+      if(globalTable.getVar(node.getNom()).taille <2) throw new RuntimeException(node.getNom()+"n'est pas un tableau");
+
+      node.tsItem = globalTable.getVar(node.getNom());
+      return null;
 
 
+    }
+
+    private boolean addFunction(SaDecFonc node) {
+        Ts localTable = new Ts();
+        String nom = node.getNom();
+
+        int nbArgs = node.getParametres() == null ? 0 : node.getParametres().length();
+        int nbVar = node.getVariable() == null ? 0 : node.getVariable().length();
+
+        SaLDec vars = node.getVariable();
+        SaDec teteVars = nbVar == 0 ? null : vars.getTete();
+
+        for (int i = 0; i < nbVar; i++) {
+            if (variableExists(teteVars.getNom(), localTable)) variableDefinedTwice(teteVars.getNom());
+            if (teteVars instanceof SaDecTab) arrayMustBeGlobabVariable();
+            localTable.addVar(teteVars.getNom(), 1);
+            if (vars.getQueue() == null) break;
+            vars = vars.getQueue();
+            teteVars = vars.getTete();
+        }
+
+        SaLDec args = node.getParametres();
+        SaDec teteParam = nbArgs == 0 ? null : args.getTete();
+        for (int i = 0; i < nbArgs; i++) {
+
+            if (variableExists(teteParam.getNom(), localTable))
+                variableDefinedTwice(teteParam.getNom());
+            if (teteParam instanceof SaDecTab) {
+                arrayMustBeGlobabVariable();
+            }
+
+            localTable.addParam(teteParam.getNom());
+
+            if (args.getQueue() == null) break;
+            args = args.getQueue();
+            teteParam = args.getTete();
+        }
+
+        node.tsItem = globalTable.addFct(nom, nbArgs, localTable, node);
+        return true;
+    }
+
+    private void arrayMustBeGlobabVariable() {
+        throw new IllegalArgumentException("les tableaux doivent être des variables globales");
+    }
+
+
+    private boolean variableExists(String nom, Ts table) {
+        return table.getVar(nom) != null;
+    }
+
+    private boolean variableDoesntExist(String nom, Ts table) {
+        return !variableExists(nom, table);
+    }
+
+    private boolean functionExists(String nom) {
+        return globalTable.getFct(nom) != null;
+    }
+
+    private void variableIsntDefined(String nom) {
+        throw new RuntimeException( nom + " n'a pas été déclarée");
+    }
+
+    private void variableDefinedTwice(String nom) {
+        throw new IllegalArgumentException("var : " + nom + " a été déclarée deux fois");
     }
 
     public Ts getTableGlobale() {
